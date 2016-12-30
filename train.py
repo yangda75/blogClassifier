4ac0e104cce76c7574005d4b4994b1640884a3ea
billyang <billyang363@gmail.com>
2016-12-30 23:45:44 +0800

    wrote train

---
+++ train.py
@@ -0,0 +1,56 @@
+import os
+import string
+files = {'teen_f': [],
+         'teen_m': [],
+         'adult_f': [],
+         'adult_m': [],
+         'mature_f': [],
+         'mature_m': [], }
+
+
+def count(file_path, vocab):
+    f = open(file_path)
+    for line in f.readlines():
+        if line.split(' ')[0][0] == '<':
+            continue
+        # rm punctuation
+        new_line = ''.join(c for c in line if c not in string.punctuation)
+        for word in new_line.split(' '):
+            if word not in vocab.keys:
+                vocab[word] = 1
+            else:
+                vocab[word] += 1
+
+
+def train(data_folder):
+    # load data
+    for root, dir_list, file_list in os.walk(data_folder):
+        for f in file_list:
+            # get tag
+            gender = f.split('.')[1]
+            age = int(f.split('.')[2])
+            if age < 20:
+                if gender == 'female':
+                    tag = 'teen_f'
+                else:
+                    tag = 'teen_m'
+            elif age < 30:
+                if gender == 'female':
+                    tag = 'adult_f'
+                else:
+                    tag = 'adult_m'
+            else:
+                if gender == 'female':
+                    tag = 'mature_f'
+                else:
+                    tag = 'mature_m'
+            files[tag].append(f)
+    # count words
+    for tag in files.keys():
+        vocab = {}
+        for f in files[tag]:
+            count(f, vocab)
+        # write the dict into a csv file
+        outfile = open(tag + '.csv')
+        [outfile.write('{0},{1}\n'.format(word, num))
+         for word, num in vocab.items()]
